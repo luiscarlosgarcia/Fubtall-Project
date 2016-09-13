e6d3c62efe2d5cf7091c0ffaf20fff7740754342
Angel Emilio <Angel Emilio@DESKTOP-MOF5D9E>
2016-09-13 12:18:37 -0400

    ejemplo

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo");
+            Console.WriteLine("Hola, mundo");
         }
     }
 }
